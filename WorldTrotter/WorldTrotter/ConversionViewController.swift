9ad61e99310017b64a8a0e208e25ade1bbd4261b
Lorenzo <ltl237@nyu.edu>
2016-02-09 15:26:15 -0500

    finished chapter 4, note decimal pad bug

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Lorenzo Llamas on 2/8/16.
+//  Copyright © 2016 FinesseFilm Productions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    var fahrenheitValue: Double? {
+        didSet {
+        updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField:UITextField){
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    func textField(textField: UITextField,
+         shouldChangeCharactersInRange range: NSRange,
+                                       replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        
+        if existingTextHasDecimalSeparator != nil &&
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+}
