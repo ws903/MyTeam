0decfa90062097d8ea511715b0f9eef015160fff
David Sim <ws903@nyu.edu>
2016-02-09 17:22:10 -0500

    3rd/4th Chapter

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by David on 2/7/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+        
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFiledEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(textField: UITextField,
+        shouldChangeCharactersInRange range: NSRange,
+        replacementString string: String) -> Bool {
+            
+            let dDC: NSCharacterSet = NSCharacterSet.letterCharacterSet()
+            
+            let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+            
+            let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+            
+            if existingTextHasDecimalSeparator != nil &&
+                replacementTextHasDecimalSeparator != nil {
+                    return false
+            }
+            else if string.rangeOfCharacterFromSet(dDC) == nil {
+                return true
+            }
+            else {
+                return false
+            }
+    }
+}
