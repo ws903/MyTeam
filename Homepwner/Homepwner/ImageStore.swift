d3b4e332907e646895c4400e860a2e5f7b34ba4f
Lorenzo <ltl237@nyu.edu>
2016-03-21 22:39:48 -0400

    finish ch15

--- Homepwner/Homepwner/ImageStore.swift
+++ Homepwner/Homepwner/ImageStore.swift
@@ -13,14 +13,51 @@ class ImageStore {
     
     func setImage(image: UIImage, forKey key: String){
         cache.setObject(image, forKey: key)
+        
+        //create full url for image
+        let imageURL = imageURLForKey(key)
+        
+        //turn image into jpeg data
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            data.writeToURL(imageURL, atomically: true)
+        }
+        
     }
     
     func imageForKey(key: String) -> UIImage? {
-        return cache.objectForKey(key) as? UIImage
+        //return cache.objectForKey(key) as? UIImage
+        if let existingImage = cache.objectForKey(key) as? UIImage {
+            return existingImage
+        }
+        let imageURL = imageURLForKey(key)
+        guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path!) else {
+            return nil
+        }
+        cache.setObject(imageFromDisk, forKey: key)
+        return imageFromDisk
     }
     
     func deleteImageForKey(key: String) {
         cache.removeObjectForKey(key)
+        
+        let imageURL = imageURLForKey(key)
+        //NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        }
+        //catch {
+          //  print("Error removing the image from disk")
+        //}
+        catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
     }
     
+    func imageURLForKey(key: String) -> NSURL {
+        let documentsDirectories =
+        NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        
+        return documentDirectory.URLByAppendingPathComponent(key)
+    }
 }
