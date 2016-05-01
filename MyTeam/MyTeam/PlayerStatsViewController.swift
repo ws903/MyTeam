2d62037492212cfc9f718bb36f36bbdff6908718
David Sim <ws903@nyu.edu>
2016-05-01 18:27:56 -0400

    FINAL WORKING APP

--- MyTeam/MyTeam/PlayerStatsViewController.swift
+++ MyTeam/MyTeam/PlayerStatsViewController.swift
@@ -13,22 +13,41 @@ class PlayerStatsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       // recentGames.layer.borderColor = UIColor.blackColor().CGColor
+       //recentGames.layer.borderWidth = 2.0
         let game1 = player.objectForKey("game1") as! [String:String]
         playerName.text = playerKey
-        assist1.text = game1["ast"]
+        fg1.text = game1["fg_percent"]
+        ft1.text = game1["ft_percent"]
         rebound1.text = game1["reb"]
+        assist1.text = game1["ast"]
+        block1.text = game1["blk"]
+        steal1.text = game1["steal"]
+        turnover1.text = game1["to"]
         points1.text = game1["pts"]
         
         let game2 = player.objectForKey("game2") as! [String:String]
         playerName.text = playerKey
-        assist2.text = game2["ast"]
+        
+        fg2.text = game2["fg_percent"]
+        ft2.text = game2["ft_percent"]
         rebound2.text = game2["reb"]
+        assist2.text = game2["ast"]
+        block2.text = game2["blk"]
+        steal2.text = game2["steal"]
+        turnover2.text = game2["to"]
         points2.text = game2["pts"]
         
         let game3 = player.objectForKey("game3") as! [String:String]
         playerName.text = playerKey
-        assist3.text = game3["ast"]
+        
+        fg3.text = game3["fg_percent"]
+        ft3.text = game3["ft_percent"]
         rebound3.text = game3["reb"]
+        assist3.text = game3["ast"]
+        block3.text = game3["blk"]
+        steal3.text = game3["steal"]
+        turnover3.text = game3["to"]
         points3.text = game3["pts"]
         
     }
@@ -37,15 +56,36 @@ class PlayerStatsViewController: UIViewController {
     
     var playerKey: String!
     
+    @IBOutlet var recentGames: UILabel!
     @IBOutlet var playerName: UILabel!
-    @IBOutlet var assist1: UILabel!
+    
+    @IBOutlet var fg1: UILabel!
+    @IBOutlet var ft1: UILabel!
     @IBOutlet var rebound1: UILabel!
+    @IBOutlet var assist1: UILabel!
+    @IBOutlet var block1: UILabel!
+    @IBOutlet var steal1: UILabel!
+    @IBOutlet var turnover1: UILabel!
     @IBOutlet var points1: UILabel!
-    @IBOutlet var assist2: UILabel!
+    
+    
+    @IBOutlet var fg2: UILabel!
+    @IBOutlet var ft2: UILabel!
     @IBOutlet var rebound2: UILabel!
+    @IBOutlet var assist2: UILabel!
+    @IBOutlet var block2: UILabel!
+    @IBOutlet var steal2: UILabel!
+    @IBOutlet var turnover2: UILabel!
     @IBOutlet var points2: UILabel!
-    @IBOutlet var assist3: UILabel!
+    
+    
+    @IBOutlet var fg3: UILabel!
+    @IBOutlet var ft3: UILabel!
     @IBOutlet var rebound3: UILabel!
+    @IBOutlet var assist3: UILabel!
+    @IBOutlet var block3: UILabel!
+    @IBOutlet var steal3: UILabel!
+    @IBOutlet var turnover3: UILabel!
     @IBOutlet var points3: UILabel!
     
     
